3e8e62095ec47a6dab501f52b13d8d29a5759095
admin <admin@61.73.115.26>
2026-01-14 23:03:17 +0900

    리스트 끝~

--- src/main/java/com/maknaez/mapper/ProductMapper.java
+++ src/main/java/com/maknaez/mapper/ProductMapper.java
@@ -10,6 +10,7 @@ public interface ProductMapper {
     public List<ProductDTO> listProduct(Map<String, Object> map); // 상품 리스트 조회
     public int dataCount(Map<String, Object> map); // 전체 상품 개수 조회
     public List<String> listCategoryNames(String categoryCode); // 사이드바 필터용 카테고리명 조회
+    public List<String> listSaleCategoryNames(); // 세일 카태고리
 
     // (기존 관리자용 메소드 유지)
     public List<CategoryDTO> listCategory();
--- src/main/java/com/maknaez/service/ProductService.java
+++ src/main/java/com/maknaez/service/ProductService.java
@@ -10,6 +10,7 @@ public interface ProductService {
     public List<ProductDTO> listProduct(Map<String, Object> map); // 상품 목록 조회
     public int dataCount(Map<String, Object> map); // 상품 개수 조회(페이징을 위햐)
     public List<String> listCategoryNames(String categoryCode); // 사이드바 필터용 카테고리 이름 목록 조회
+    public List<String> listSaleCategoryNames(); // 세일 카태고리
     
     // (기존 관리자용 메소드 유지)
     public List<CategoryDTO> listCategory();
